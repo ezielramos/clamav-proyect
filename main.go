cf8644c470097bebc24f0d2c11c2dc311c8578b2
ezielramos <47543896+ezielramos@users.noreply.github.com>
2023-01-07 03:11:08 -0500

    ready for testing

--- main.go
+++ main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("start scaning...")
+
+	r := gin.Default()
+
+	r.GET("/api/ping", PingResponse)
+	r.POST("api/scan", ScanArchive)
+
+	if err := r.Run(":8080"); nil != err {
+		panic(err.Error())
+	}
+}
+
+func ScanArchive(c *gin.Context) {
+
+	err := c.Request.ParseForm()
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	} else {
+
+		path := c.Request.FormValue("file")
+
+		form := new(bytes.Buffer)
+		writer := multipart.NewWriter(form)
+		fw, err := writer.CreateFormFile(path, filepath.Base(path))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fd, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fd.Close()
+		_, err = io.Copy(fw, fd)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writer.Close()
+
+		client := &http.Client{}
+		req, err := http.NewRequest("POST", "http://localhost:9000/scan", form)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		bodyText, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%s\n", bodyText)
+
+	}
+	c.String(http.StatusOK, "scan succes")
+}
+
+func PingResponse(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
 }
